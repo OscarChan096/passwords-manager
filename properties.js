5d23d7e18791e262503f642e11a108c7bb055753
Oscar Chan <oscarchan96@outlook.com>
2025-01-31 01:43:42 -0600

    fix: BASE_URL_PWD

--- properties.js
+++ properties.js
@@ -1,4 +1,3 @@
 export const DATA = {
-    BASE_URL_PWD: `http://127.0.0.1:5000/api/pwd/`,
-    //URL_SERVER_REMOTE: `http://192.168.1.119:5000/api/pwd/`,
+    BASE_URL_PWD: `http://192.168.1.119:5000/api/pwd/`
 }
